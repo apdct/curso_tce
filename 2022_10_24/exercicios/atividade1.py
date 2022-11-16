7f405f34461e59834dd8f7240d3b12b1c164defa
Ana PC <118301029+apdct@users.noreply.github.com>
2022-11-16 10:38:21 -0300

    Add files via upload

---
+++ 2022_10_24/exercicios/atividade1.py
@@ -0,0 +1,15 @@
+"""
+Entrada de dados por atribuição de variáveis.
+Requisito: Crie um programa que entre os números 2 e 3 na memória do computador.
+Autor: Ana Paula C.
+Versão: 0.0.1
+"""
+
+# Entrada
+numero_1 = 2
+numero_2 = 3
+
+# Processamento
+
+
+# Saída
---
+++ 2022_10_24/exercicios/atividade2.py
@@ -0,0 +1,16 @@
+"""
+Programa de saída de dados.
+Requisito: Faça um programa que apresente na tela os números 2 e 3.
+Autor: Ana Paula C.
+Versão: 0.0.1
+"""
+
+# Entrada
+numero_1 = 2
+numero_2 = 3
+
+# Processamento
+
+
+# Saída
+print(f"\nNúmeros neste programa: {numero_1} e {numero_2}.")
