d581e0443ed788a8af6cd5c0327a3d0468874bc9
Ana PC <118301029+apdct@users.noreply.github.com>
2022-11-16 10:39:09 -0300

    Add files via upload

---
+++ 2022_10_26/exercícios/exercicio03.py
@@ -0,0 +1,20 @@
+"""
+Programa Soma
+Requisito: Faça um programa que leia dois números inteiros, calcule a soma deles
+e imprima o resultado na tela.
+Autor: Ana Paula C.
+Data: 15/11/2022
+Versão: 0.0.1
+"""
+
+# Entrada
+print("Este programa soma dois números inteiros.\n")
+numero_1 = int(input("Digite o primeiro número: "))
+numero_2 = int(input("Digite o segundo número: "))
+
+# Processamento
+soma = numero_1 + numero_2
+
+# Saída
+
+print(f"\nA soma dos números {numero_1} e {numero_2} é igual a {soma}.")
---
+++ 2022_10_26/exercícios/exercicio04.py
@@ -0,0 +1,18 @@
+"""
+Cálculo de passagem
+Requisito: Faça um programa que calcule o valor gasto com passagem de ônibus de
+um grupo de 20 pessoas, sabendo que o preço unitário da passagem é R$ 7,30.
+Autor: Ana Paula C.
+Data: 15/10/2022
+Versão: 0.0.1
+"""
+
+# Entrada
+valor_passagem = 7.30
+pessoas = 20
+
+# Processamento
+total = valor_passagem * pessoas
+
+# Saída
+print(f"Valor gasto em passagens pelo grupo de 20 pessoas é R$ {total}.")
